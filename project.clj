166b251d88215c07991f4c5b59b191aab712226f
filip <fbielejec@gmail.com>
2019-07-22 21:23:32 +0200

    slimer

--- project.clj
+++ project.clj
@@ -7,17 +7,48 @@
                  [org.clojure/clojurescript "1.10.439"]
                  [re-frame "0.10.8"]]
 
-  :doo {:paths {:karma "./node_modules/karma/bin/karma"}}
+  :doo {:paths {:karma "./node_modules/karma/bin/karma"
+                :slimer "./node_modules/.bin/slimerjs"}
+        :karma {:config {
 
-  :npm {:devDependencies [[karma "1.7.1"]
-                          [karma-chrome-launcher "2.2.0"]
-                          [karma-cli "1.0.1"]
-                          [karma-cljs-test "0.1.0"]]}
+                         ;; "frameworks" ["es6-shim" "requirejs"]
+
+                         "files" ["node_modules/es6-shim/es6-shim.js"
+                                  {"pattern" "./node_modules/babel-polyfill/browser.js"
+                                   "instrument" false}
+                                  ]
+
+                         }}
+        }
+
+  :npm {:devDependencies [
+
+                          "karma" "1.7.0",
+                          "karma-chrome-launcher" "2.1.1",
+                          "karma-cljs-test" "0.1.0",
+                          "karma-electron-launcher" "0.2.0",
+                          "karma-firefox-launcher" "1.1.0",
+                          "karma-opera-launcher" "1.0.0",
+                          "karma-phantomjs-launcher" "1.0.4",
+                          "karma-safari-launcher" "1.0.0",
+                          "karma-slimerjs-launcher" "1.0.0",
+                          "phantomjs-prebuilt" "2.1.14",
+                          "slimerjs" "0.10.3"
+
+                          ;; [karma "2.0.0"]
+                          ;; [karma-es6-shim "1.0.0"]
+                          ;; [karma-chrome-launcher "3.0.0"]
+                          ;; [karma-phantomjs-launcher "1.0.4"]
+                          ;; [karma-slimerjs-launcher "1.1.0"]
+                          ;; [karma-cli "2.0.0"]
+                          ;; [karma-cljs-test "0.1.0"]
+
+                          ]}
 
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.9.0"]
                                   [day8.re-frame/test "0.1.5"]]
                    :plugins [[lein-cljsbuild "1.1.7"]
-                             [lein-doo "0.1.8"]
+                             [lein-doo "0.1.11"]
                              [lein-npm "0.6.2"]]}}
 
   :deploy-repositories [["snapshots" {:url "https://clojars.org/repo"
