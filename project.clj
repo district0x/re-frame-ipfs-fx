1d1409e5bed074ee14960f1d6f55c32ca6919284
filip <fbielejec@gmail.com>
2019-07-22 21:28:16 +0200

    no idea whats going on

--- project.clj
+++ project.clj
@@ -7,43 +7,15 @@
                  [org.clojure/clojurescript "1.10.439"]
                  [re-frame "0.10.8"]]
 
-  :doo {:paths {:karma "./node_modules/karma/bin/karma"
-                :slimer "./node_modules/.bin/slimerjs"}
-        :karma {:config {
+  :doo {:paths {:karma "./node_modules/karma/bin/karma"}}
 
-                         ;; "frameworks" ["es6-shim" "requirejs"]
-
-                         "files" ["node_modules/es6-shim/es6-shim.js"
-                                  {"pattern" "./node_modules/babel-polyfill/browser.js"
-                                   "instrument" false}
-                                  ]
-
-                         }}
-        }
-
-  :npm {:devDependencies [
-
-                          "karma" "1.7.0",
-                          "karma-chrome-launcher" "2.1.1",
-                          "karma-cljs-test" "0.1.0",
-                          "karma-electron-launcher" "0.2.0",
-                          "karma-firefox-launcher" "1.1.0",
-                          "karma-opera-launcher" "1.0.0",
-                          "karma-phantomjs-launcher" "1.0.4",
-                          "karma-safari-launcher" "1.0.0",
-                          "karma-slimerjs-launcher" "1.0.0",
-                          "phantomjs-prebuilt" "2.1.14",
-                          "slimerjs" "0.10.3"
-
-                          ;; [karma "2.0.0"]
-                          ;; [karma-es6-shim "1.0.0"]
-                          ;; [karma-chrome-launcher "3.0.0"]
-                          ;; [karma-phantomjs-launcher "1.0.4"]
-                          ;; [karma-slimerjs-launcher "1.1.0"]
-                          ;; [karma-cli "2.0.0"]
-                          ;; [karma-cljs-test "0.1.0"]
-
-                          ]}
+  :npm {:devDependencies [karma "2.0.0"
+                          karma-es6-shim "1.0.0"
+                          karma-chrome-launcher "3.0.0"
+                          karma-phantomjs-launcher "1.0.4"
+                          karma-slimerjs-launcher "1.1.0"
+                          karma-cli "2.0.0"
+                          karma-cljs-test "0.1.0"]}
 
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.9.0"]
                                   [day8.re-frame/test "0.1.5"]]
